5bb47aff57bed0a7426deeb99e88dd1f4dab618f
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2023-08-02 16:22:50 +0300

    Add files via upload

---
+++ RepeatingNumber.java
@@ -0,0 +1,29 @@
+package java101;
+
+import java.util.Arrays;
+
+public class RepeatingNumber {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 21, 31, 43, 24, 34, 22, 24, 54, 76, -54, 56, 34, 0, 234 };
+		System.out.println("Dizi : " + Arrays.toString(numbers));
+
+		Arrays.sort(numbers);
+		System.out.println("Sıralı : " + Arrays.toString(numbers));
+
+		int counter = 0;
+		for (int i = 0; i < numbers.length - 1; i++) {
+			if (numbers[i] == numbers[i + 1]) {
+				int evenNumber = numbers[i];
+				if (evenNumber % 2 == 0) {
+					counter++;
+					System.out.print(evenNumber + " ");
+				}
+			}
+		}
+		if (counter == 0) {
+			System.out.println("Tekrar Eden Çift Sayı Bulunamadı...:)");
+		}
+	}
+}
